377290cf4c4b9f9dc927d56b9a454b2ebbba1c63
getzch <keepprogress@gmail.com>
2020-11-22 22:50:37 +0800

    follow solution to add read file function

--- src/com/luv2code/springdemo/COMPUTFortuneService.java
+++ src/com/luv2code/springdemo/COMPUTFortuneService.java
@@ -1,32 +1,59 @@
 package com.luv2code.springdemo;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+
 
 @Component
 public class COMPUTFortuneService implements FortuneService {
-	private String[] data = {
-			"Beware the thing behind you",
-			"Today definitely your day",
-			"Don't go out today"
-	};
 	
+	private String fileName = "C:/workspace_for_javaEEEclipse/spring-demo-annotations/src/sport-fortune";
+	private List<String> theFortunes;
+	
+	// create a random number generator
 	Random myRandom = new Random();
 	
-	@Value("${foo.email}")
-	private String email;
-	
-	@Value("${foo.teamName}")
-	private String teamName;
+	public COMPUTFortuneService() {
+		
+		File theFile = new File(fileName);
+		
+		System.out.println("Reading fortunes from file: " +theFile);
+		System.out.println("File exists: " + theFile.exists());
+		
+		// initialize array list
+		theFortunes = new ArrayList<String>();
+		
+		//read fortune from file
+		try (BufferedReader br = new BufferedReader(
+				new FileReader(theFile))) {
+			
+			String tempLine;
+			
+			while ((tempLine = br.readLine()) != null) {
+				theFortunes.add(tempLine);
+			}
+			
+		}  catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 
 	
 	@Override
 	public String getFortune() {
+		// pick a random string from the array
+		int index = myRandom.nextInt(theFortunes.size());
 		
-		int index = myRandom.nextInt(data.length);
-		return data[index] + "\n" + email + "\n" + teamName;
+		String tempFortune = theFortunes.get(index);
+		
+		return tempFortune;
 	}
 
 }
