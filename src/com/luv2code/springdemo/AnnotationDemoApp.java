3ca336527c11f1a31c6b3c0fd4a6cbca68c2b4ba
getzch <keepprogress@gmail.com>
2020-11-22 20:59:18 +0800

    complete the section8 practice Qualifier for Dependency Injection

--- src/com/luv2code/springdemo/AnnotationDemoApp.java
+++ src/com/luv2code/springdemo/AnnotationDemoApp.java
@@ -12,7 +12,6 @@ public class AnnotationDemoApp {
 		
 		// get the bean from spring container
 		Coach theCoach = context.getBean("tennisCoach", Coach.class);
-		
 		// call a method on the bean
 		System.out.println(theCoach.getDailyWorkout());
 		
