c0a8e670b0e040fe9252a582c52511fb0693ac11
nick800103 <keepprogress@gmail.com>
2020-11-23 18:11:35 +0800

    Update TennisCoach.java

--- src/com/luv2code/springdemo/TennisCoach.java
+++ src/com/luv2code/springdemo/TennisCoach.java
@@ -3,18 +3,27 @@ package com.luv2code.springdemo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
+import org.springframework.beans.factory.annotation
 
 @Component
 public class TennisCoach implements Coach {
 	
 	@Autowired
-	@Qualifier("randomFortuneService")
+	@Qualifier("happyFortuneService")
 	private FortuneService fortuneService;
 	
 	// define a default constructor
 	public TennisCoach() {
 		System.out.println(">> TennisCoach: inside default constructor");
 	}
+	
+	// define my init method
+	@PostConstruct
+	public void doMyStartupStuff() {
+		System.out.println(">> tennisCoach: inside of doMyStartupStuff()");
+	}
+	
+	// define my destroy method
 	
 	/*
 	@Autowired
