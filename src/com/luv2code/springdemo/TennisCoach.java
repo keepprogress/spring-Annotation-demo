388e1fdedd153a61109cf6b48798eeb822473c8c
getzch <keepprogress@gmail.com>
2020-11-24 21:37:53 +0800

    fixed type error

--- src/com/luv2code/springdemo/TennisCoach.java
+++ src/com/luv2code/springdemo/TennisCoach.java
@@ -1,9 +1,10 @@
 package com.luv2code.springdemo;
 
+import javax.annotation.PostConstruct;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
-import org.springframework.beans.factory.annotation
 
 @Component
 public class TennisCoach implements Coach {
