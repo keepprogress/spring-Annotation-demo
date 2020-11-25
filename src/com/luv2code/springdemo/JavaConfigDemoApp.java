7747823cacd6678a5db24ea8194d26d66386af13
getzch <keepprogress@gmail.com>
2020-11-25 09:14:07 +0800

    fixed type error

--- src/com/luv2code/springdemo/JavaConfigDemoApp.java
+++ src/com/luv2code/springdemo/JavaConfigDemoApp.java
@@ -9,7 +9,7 @@ public class JavaConfigDemoApp {
 
 		// read spring config file
 		AnnotationConfigApplicationContext context =
-				new AnnotationConfigApplicationContext("SportConfig.class");
+				new AnnotationConfigApplicationContext(SportConfig.class);
 		
 		// get the bean from spring container
 		Coach theCoach = context.getBean("tennisCoach", Coach.class);
